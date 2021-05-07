1b7c0788efa66ece21b1f7f3dfa5a6a5c207a329
Nick Fuller <nmfuller@verizon.net>
2021-05-06 22:37:31 -0400

    Finalized the layout for the scoreboard fragment layout. Organized the RadioGroup index and local/global button values into      their own unique values for easier FireStore querying later.      - Still need to organize the 2 columns and 9 table rows indexes into      a 2D array for easier data displaying after querying.

--- app/src/main/java/com/example/matchappproject/ScoreboardFragment.java
+++ app/src/main/java/com/example/matchappproject/ScoreboardFragment.java
@@ -1,29 +1,65 @@
 package com.example.matchappproject;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class ScoreboardFragment extends Fragment {
+    Button refresh, local, global;
+    int locGlob = 0;    // If 0, local scores. If 1, global scores.
+    int radioIndex = 0; // 0 for easy, 1 for medium, 2 for hard.
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        View view = inflater.inflate(R.layout.fragment_scoreboard, container, false);
+
+        RadioGroup radioGroup = (RadioGroup) view.findViewById(R.id.radioGroup_scoreboard);
+        radioIndex = radioGroup.getCheckedRadioButtonId();
+        Log.i("scoreboard", "radio index " + radioIndex + " selected.");
+        
+        local = view.findViewById(R.id.button_local);
+        local.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locGlob = 0;
+            }
+        });
+
+        global = view.findViewById(R.id.button_global);
+        global.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locGlob = 1;
+            }
+        });
+
+        refresh = view.findViewById(R.id.button_refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Insert code here to requery data results
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scoreboard, container, false);
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_sc2menu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(ScoreboardFragment.this)
